ea91f1fd79ca790e770fd4ac313959dfc8b83423
merve çalışır <m-r-v-c-96@hotmail.com>
2021-02-23 22:10:18 +0300

    EKLEME

---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.FirstName).MinimumLength(2);
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Password).MinimumLength(10).WithMessage("Paralonız en az 10 karakter olmalıdır.");
+            RuleFor(u => u.Email).EmailAddress();
+        }
+    }
+}
